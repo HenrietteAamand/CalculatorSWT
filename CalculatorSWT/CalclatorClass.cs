2a6a30c36e53c1aa19590c5ddf5b429e9a885dc1
Henriette Holm <henriette.aamand.holm@live.dk>
2021-02-04 13:07:28 +0100

    Add project files.

---
+++ CalculatorSWT/CalclatorClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class CalclatorClass
+    {
+        public double Add(double a, double b)
+        {
+            double result;
+            result = a + b;
+
+            return result;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            double result;
+            result = a - b;
+
+            return result;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            double result;
+            result = a*b;
+
+            return result;
+        }
+
+        public double Power(double x, double exp)
+        {
+            double result;
+            result = Math.Pow(x, exp);
+
+            return result;
+        }
+    }
+}
---
+++ CalculatorSWT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorSWT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
