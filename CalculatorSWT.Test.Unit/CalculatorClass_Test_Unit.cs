0e6217448d9668b68b40728a9cebfb1bac517107
Henriette Holm <henriette.aamand.holm@live.dk>
2021-02-08 14:56:32 +0100

    Tilføjet testprojekt + en test

---
+++ CalculatorSWT.Test.Unit/CalculatorClass_Test_Unit.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Calculator;
+
+namespace CalculatorSWT.Test.Unit
+{
+    public class CalculatorClass_Test_Unit
+    {
+        private CalclatorClass uut;
+        [SetUp]
+        public void Setup()
+        {
+            uut = new CalclatorClass();
+        }
+
+        [Test]
+        public void Add_2And4_result6()
+        {
+            double result = uut.Add(2, 4);
+            Assert.That(result, Is.EqualTo(6));
+        }
+    }
+}
